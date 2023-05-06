2dd037ab28d3689db8437e43b668dcc12416a456
Bo Latt <bolattny@gmail.com>
2023-05-06 19:48:22 -0400

    add header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Events from "./components/Events";
 import bootstrap from "bootstrap";
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
+    <div className="app">
+      <Header />
       <Events />
     </div>
   );
