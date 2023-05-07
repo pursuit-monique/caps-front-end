28be3429fce1a83c382fb6191ecb31c7b09a50c7
pursuit-monique <moniquecorrea@pursuit.orgname>
2023-05-07 18:10:03 -0400

    index page

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -5,11 +5,37 @@ export default function EventCard() {
 
     return (
     <>
-         <container className="container">
- <div className="cardSize">
+      <container className="container d-flex flex-column overflow-auto">
+        <div className="cardSize">
+          <div className="rowimg">
+            <div className="col-1 imgContainer">
+        <img src="https://picsum.photos/200/600" className="imageContain" alt="..." />
+        </div>
+  <div className="col constraint">
+      <div className="row-1"><h1 className="title">Title</h1></div>
+      <div className="row-2 cardInfo">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in nunc faucibus, dignissim urna ac, aliquam est. 
+      </div>
+      <div className="row align-items-center">
+        <i className="userIcon margin"><span className="indicator"></span></i>
+        <div className="col">
+            <div className="row infoText">July Gold</div>
+            <div className="row orgText infoText">Pursuit</div>
+        </div>
+        <div className='col-1'></div>
+        <div className="col"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-geo-alt-fill mainColor" viewBox="0 0 16 16">
+  <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+</svg><span className="orgText infoText">0.6km away</span></div>
+        </div>
+  </div>
+</div>
+</div>
+
+
+<div className="cardSize">
   <div className="rowimg">
   <div className="col-1 imgContainer">
-    <img src="https://picsum.photos/200/600" className="imageContain" alt="..." />
+    <img src="https://loremflickr.com/640/360" className="imageContain" alt="..." />
   </div>
   <div className="col constraint">
       <div className="row-1"><h1 className="title">Title</h1></div>
@@ -35,7 +61,7 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in nunc faucibus
 <div className="cardSize">
   <div className="rowimg">
   <div className="col-1 imgContainer">
-    <img src="https://picsum.photos/200/600" className="imageContain" alt="..." />
+    <img src="https://placebeard.it/640x360" className="imageContain" alt="..." />
   </div>
   <div className="col constraint">
       <div className="row-1"><h1 className="title">Title</h1></div>
@@ -61,7 +87,7 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in nunc faucibus
 <div className="cardSize">
   <div className="rowimg">
   <div className="col-1 imgContainer">
-    <img src="https://picsum.photos/200/600" className="imageContain" alt="..." />
+    <img src="https://placebeard.it/640x360" className="imageContain" alt="..." />
   </div>
   <div className="col constraint">
       <div className="row-1"><h1 className="title">Title</h1></div>
@@ -78,6 +104,32 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in nunc faucibus
         <div className="col"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-geo-alt-fill mainColor" viewBox="0 0 16 16">
   <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
 </svg><span className="orgText infoText">0.6km away</span></div>
+        </div>
+  </div>
+</div>
+</div>
+
+
+<div className="cardSize">
+  <div className="rowimg">
+  <div className="col-1 imgContainer">
+    <img src="https://placebeard.it/640x360" className="imageContain" alt="..." />
+  </div>
+  <div className="col constraint">
+      <div className="row-1"><h1 className="title">Title</h1></div>
+      <div className="row-2 cardInfo">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in nunc faucibus, dignissim urna ac, aliquam est. 
+      </div>
+      <div className="row align-items-center">
+        <i className="userIcon margin"><span className="indicator"></span></i>
+        <div className="col">
+            <div className="row infoText">July Gold</div>
+            <div className="row orgText infoText">Pursuit</div>
+        </div>
+        <div className='col-1'></div>
+        <div className="col"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-geo-alt-fill mainColor" viewBox="0 0 16 16">
+  <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+</svg><span className="orgText infoText">0.6km <span className="hidden">away</span></span></div>
         </div>
   </div>
 </div>
---
+++ src/components/Menu.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../custom.css';
+
+export default function Menu(){
+return (
+    <>
+<div class="topnav">
+  <a class="active" href="#home">My Events</a>
+  <a href="#news">Previous Events</a>
+  <a href="#contact">All Events</a>
+</div>
+
+</>
+)
+}
