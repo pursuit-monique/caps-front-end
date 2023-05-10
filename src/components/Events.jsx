75a685a24b2e16433cd90da9869c3269e6968ac6
pursuit-monique <moniquecorrea@pursuit.orgname>
2023-05-10 18:43:57 -0400

    legend added for real this time

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -13,6 +13,14 @@ export default function Events() {
             <div className="flex-column justify-content-end "></div>
             <div className="flex-column heightmenu overflow-auto order-2"><EventCard /></div>
             {/* <div className="order-1 justify-content-center"> <img src="https://snazzy-maps-cdn.azureedge.net/assets/132-light-gray.png?v=20170626081135" className="map" alt="..." /></div> */}
+            <div className="legend">
+                <div class="card">
+                    <div class="card-body">
+                        <h5>Legend</h5>
+                        You: <img src="../locationsm.svg" alt="" />
+                    </div>
+                </div>
+            </div>
             <div className="order-1 justify-content-center">  <Map /> </div>
         </article>
     </>
