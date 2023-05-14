bb35fcac9805acebb35bff79183607acc7b45ee1
pursuit-monique <moniquecorrea@pursuit.orgname>
2023-05-14 16:33:01 -0400

    Using dummy data to test other features

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Events from "./components/Events";
 import "./App.css";
 import Header from "./components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
   return (
--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,27 +1,27 @@
+import {useState} from "react";
+
 import EventCard from './EventCard';
 import Menu from "./Menu";
 import Map from "./Map"
-
+import Legend from "./Legend"
+// import EventsComponent from "./Generator"
 import '../custom.css';
 
 export default function Events() {
+    const [info, setInfo] = useState({lat: null, lng: null, name: null, pict: null});
+
+
     return (
     <> 
         {/* <NavBar /> */}
         <Menu />
+        {/* <EventsComponent /> */}
         <article className="d-flex flex-wrap body">
             <div className="flex-column justify-content-end "></div>
             <div className="flex-column heightmenu overflow-auto order-2"><EventCard /></div>
             {/* <div className="order-1 justify-content-center"> <img src="https://snazzy-maps-cdn.azureedge.net/assets/132-light-gray.png?v=20170626081135" className="map" alt="..." /></div> */}
-            <div className="legend">
-                <div class="card">
-                    <div class="card-body">
-                        <h5>Legend</h5>
-                        You: <img src="../locationsm.svg" alt="" />
-                    </div>
-                </div>
-            </div>
-            <div className="order-1 justify-content-center">  <Map /> </div>
+            <Legend info={info}/>
+            <div className="order-1 justify-content-center">  <Map info={{setInfo}} /> </div>
         </article>
     </>
     )
