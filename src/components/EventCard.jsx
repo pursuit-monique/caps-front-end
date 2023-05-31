36267ec295fea5d14930bef8f66744bcf17d2cad
pursuit-monique <moniquecorrea@pursuit.orgname>
2023-05-31 12:33:12 -0400

    Reset button

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -3,7 +3,6 @@ import '../custom.css';
 
 export default function EventCard({currEvents, mapCenter}) {
 
-  console.log(currEvents)
   function parseTitle(title){
 
     if (title.length > 45){
@@ -38,42 +37,40 @@ export default function EventCard({currEvents, mapCenter}) {
     return (
     <>
       <container className="container d-flex flex-column overflow-auto">
-{currEvents.map(event =>  (
-  <div className="cardSize">
-          <div className="rowimg">
-            <div className="col-1 imgContainer">
-        {/* <img src={event.img_link} className="imageContain" alt="..." /> */}
-        <div
-          className="imageContain"
-          style={{
-            backgroundImage: `linear-gradient(to right, transparent 85%, white), url(${event.img_link})`,
-            // backgroundImage: `url(${event.img_link})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            }}></div>
-        </div>
-  <div className="col constraint">
-      <div className="row-1"><h1 className="title">{parseTitle(event.title)} {}</h1></div>
-      <div className="row-2 cardInfo">
-{event.description}
+    {currEvents.map(event =>  (
+      <div className="cardSize">
+              <div className="rowimg">
+                <div className="col-1 imgContainer">
+            <div
+              className="imageContain"
+              style={{
+                backgroundImage: `linear-gradient(to right, transparent 85%, white), url(${event.img_link})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                }}></div>
+            </div>
+      <div className="col constraint">
+          <div className="row-1"><h1 className="title">{parseTitle(event.title)} {}</h1></div>
+          <div className="row-2 cardInfo">
+    {event.description}
+          </div>
+          <div className="row align-items-center">
+          <span className="indicator"></span><img src={event.user_profile_link} alt="name" className="userIcon margin"></img>
+            <div className="col">
+                <div className="row infoText">{event.f_name} {event.l_name}</div>
+                <div className="row orgText infoText">Pursuit</div>
+            </div>
+            <div className='col-1'></div>
+            <div className="col"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-geo-alt-fill mainColor" viewBox="0 0 16 16">
+      <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+    </svg><span className="orgText infoText">{calculateDistance( Number(mapCenter.lat), Number(mapCenter.lng), Number(event.latitude), Number(event.longitude))}mi away</span></div>
+            </div>
       </div>
-      <div className="row align-items-center">
-      <span className="indicator"></span><img src={event.user_profile_link} alt="name" className="userIcon margin"></img>
-        <div className="col">
-            <div className="row infoText">{event.f_name} {event.l_name}</div>
-            <div className="row orgText infoText">Pursuit</div>
-        </div>
-        <div className='col-1'></div>
-        <div className="col"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-geo-alt-fill mainColor" viewBox="0 0 16 16">
-  <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
-</svg><span className="orgText infoText">{calculateDistance( Number(mapCenter.lat), Number(mapCenter.lng), Number(event.latitude), Number(event.longitude))}mi away</span></div>
-        </div>
-  </div>
-</div>
-</div>
-))}
+    </div>
+    </div>
+    ))}
 
-        </container> 
+            </container> 
     </>
     )
 }
--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -8,6 +8,8 @@ import Map from "./Map"
 import Legend from "./Legend"
 import Categories from "./Categories"
 import CategoriesCounter from "./CategoriesCounter";
+import Reset from "./Reset";
+
 import '../custom.css';
 
 const API = process.env.REACT_APP_EVENTS_URL;
@@ -15,7 +17,7 @@ export default function Events() {
     const [mapCenter, setMapCenter] = useState({ lat: 0, lng: 0 });
 
     //State for Category buttons
-    const [category, setCategory] = useState("");
+    const [category, setCategory] = useState('');
 
 
     //State for Map and Card Data
@@ -78,23 +80,24 @@ export default function Events() {
         {/* Category selection bar */}
         <Categories setCategory={setCategory} />
         <CategoriesCounter currEvents={currEvents} category={category} />
+        {/* <Reset setCategory={setCategory} category={category} /> */}
 
         <article className="d-flex flex-wrap body">
 
             <div className="flex-column justify-content-end "></div>
 
-            {/* Event card display */}
-            <div className="flex-column heightmenu overflow-auto order-2">
-                <EventCard currEvents={currEvents.filter( event => !!category.id ? event.cause_id === Number(category.id) : true)} mapCenter={mapCenter} />
-            </div>
+                {/* Event card display */}
+                <div className="flex-column heightmenu overflow-auto order-2">
+                    <EventCard currEvents={currEvents.filter( event => !!category.id ? event.cause_id === Number(category.id) : true)} mapCenter={mapCenter} />
+                </div>
 
-            {/* Legend:  Is display: hidden on mediaScreen width < 480px */}
-            <Legend category={category}  />
+                {/* Legend:  Is display: hidden on mediaScreen width < 480px */}
+                <Legend category={category}  />
 
-            {/* Map display.   */}
-            <div className="order-1 justify-content-center">  
-                <Map currEvents={currEvents.filter( event => !!category.id ? event.cause_id === Number(category.id) : true)} category={category} mapCenter={mapCenter} />
-            </div>
+                {/* Map display.   */}
+                <div className="order-1 justify-content-center">  
+                    <Map currEvents={currEvents.filter( event => !!category.id ? event.cause_id === Number(category.id) : true)} category={category} mapCenter={mapCenter} />
+                </div>
 
         </article>
     </>
