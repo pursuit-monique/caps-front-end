fc71f3add50cd23476fd61e8e879deef35f62036
Monique <107360758+pursuit-monique@users.noreply.github.com>
2023-06-16 18:10:07 -0400

    Modifications

--- src/pages/Event2.jsx
+++ src/pages/Event2.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect, useContext } from "react";
 import { useParams, useNavigate} from "react-router-dom";
-// import ShareThis from "../components/helpers/ShareThis"
+import ShareThis from "../components/helpers/ShareThis"
 import axios from "axios"; 
 import { AuthContext } from "../context/AuthContext";
 
-// import {cause, tempData} from "../components/helpers/objects"
+import {cause, tempData} from "../components/helpers/objects"
 // import {checkin, dateHandler} from "../components/helpers/functions"
 import './Event.css';
 import '../custom.css';
@@ -14,16 +14,14 @@ import DirectionService from "../components/DirectionService";
 
 
 function Event2() {
+  // const API = process.env.REACT_APP_BACKEND_URL;
   // const API = "https://happn.onrender.com";
   const API = process.env.REACT_APP_BACKEND_URL;
-  // const API = process.env.REACT_APP_LOCAL_BACKEND;
   const { currentUser } = useContext(AuthContext);
   const { id } = useParams();
   const navigate = useNavigate();
   const [event, setEvent] = useState("");
   const [livestreams, setLivestreams] = useState([]);
-  const [loading, setLoading] = useState(false);
-
   console.log("backend", API);
   // get event details
   useEffect(() => {
@@ -33,7 +31,9 @@ function Event2() {
         console.log(res.data);
         setEvent(res.data);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => { setEvent(tempData[id -1])
+        // console.log(event)
+      });
 
     // get live streams by event id
     axios
@@ -51,20 +51,19 @@ function Event2() {
         `${API}/events/${event.id}/checkin/${currentUser.uid}`
       );
       console.log(res);
-      window.location.reload(false);
     } catch (err) {
       console.log(err);
     }
   }
 
   async function handleLive() {
-    setLoading(true);
+    // const roomCodes = await axios.post(`${API}/live/create-room`);
     const res = await axios.post(`${API}/live/create-room`, {
       event_id: event.id,
       streamer_user_id: currentUser.uid,
     });
-    setLoading(false);
-
+    // add user info to backend here
+    // const broadcasterCode = await res.data.roomCodes.data[0].code;
     console.log("response", res);
     const broadcasterCode = await res.data.broadcaster_code;
     // console.log(broadcasterCode);
@@ -74,9 +73,9 @@ function Event2() {
   function joinLive(viewerCode) {
     navigate("/live/" + viewerCode);
   }
-
+const eventFormat = event ? event.address.split(',') : '';
+const formattedEventAddress =  eventFormat ? `${eventFormat[0]}, ${eventFormat[1]}, ${eventFormat[2]}, ${event.zip}` : ''
   return (
-
     <>
     <container 
     style={{
@@ -88,7 +87,6 @@ function Event2() {
     backgroundSize: 'cover',
     backgroundAttachment: 'fixed',
 }}>
-
     <div 
     className="container nomargin"
     // style={{backgroundImage:`url(${cityscape})`,
@@ -98,9 +96,8 @@ function Event2() {
     >
       
       {/* <Header /> */}
-      {loading && <Loader />}
       <div className="row gx-4 gx-lg-5 my-5">
-        <div className="col-md-7">
+        <div className="col-md-7 align-middle">
           {/* <img
             className="img-fluid rounded mb-4 mb-lg-0"
             src="https://dummyimage.com/900x400/dee2e6/6c757d.jpg"
@@ -110,6 +107,7 @@ function Event2() {
             className="img-fluid rounded mb-4 mb-lg-0"
             // src="https://picsum.photos/400/300"
             src={event.img_link}
+            style={{backgroundColor: "white", borderRadius: '10px', border: '1px solid rgb(204, 210, 219)', boxShadow: '0 0 5px 2px rgba(0, 0, 0, 0.2)'}}
             alt="event cover"
           />
         </div>
@@ -124,14 +122,16 @@ function Event2() {
           <a className="btn btn-primary" href="#!">
             Call to Action!
           </a> */}
-          <div className="event-details-container">
-            <div className="event-title-wrapper">
-              <h3 className="event-title">{event.title}</h3>
+          <div className="event-details-container"   style={{backgroundColor: "white", borderRadius: '10px', border: '1px solid rgb(204, 210, 219)', boxShadow: '0 0 5px 2px rgba(0, 0, 0, 0.2)'}}>
+            <div className="event-title-wrapper" style={{backgroundColor: `${event.cause_id === 'undefined' ? cause[event.cause_id][1] : '#03bdd6'}}`, borderRadius: '10px 10px 0px 0px'}}>
+            {/* <div className="event-title-wrapper" style={{backgroundColor: cause[event.cause_id][1]}}> */}
+              <h3 className="event-title" style={{ marginLeft: '20px', marginTop: '2px'}}>{event.title}</h3>
             </div>
-            <div className="event-creator-container">
+            <p style={{textAlign: 'right', color: '#545454', fontSize: "small", marginTop: '8px', marginRight: '8px'}}><em>{formattedEventAddress}</em></p>
+            <div className="event-creator-container" style={{paddingTop: '8px', marginTop: '8px'}}>
               <img
-                className="avatar"
-                src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+                className="avatar organizer"
+                src={event.user_profile_link || "https://t3.ftcdn.net/jpg/03/53/11/00/360_F_353110097_nbpmfn9iHlxef4EDIhXB1tdTD0lcWhG9.jpg"}
                 alt="Avatar"
               />
               <p>
@@ -139,19 +139,23 @@ function Event2() {
               </p>
 
               <div className="event-categories">
-                <ul>
+                <ul 
+                // style={{ marginLeft: '88px'}}
+                >
+                  <li style={{backgroundColor: `${event.cause_id === 'undefined' ? cause[event.cause_id][1] : '#03bdd6'}`, marginBottom: '8px'}}>{ event ? cause[event.cause_id][0] : 'blah'}</li>
                   <li>{event.category}</li>
                 </ul>
               </div>
             </div>
-            <p className="event-description">{event.description}</p>
-            <p className="event-date">
-              Date: {event.date?.split("T")[0]} {"  "} Time:{" "}
-              {event.time?.split(":").slice(0, 2).join(":")}
-            </p>
 
+            <p className="event-description" style={{borderLeft: `2px solid ${event.cause_id === 'undefined' ? cause[event.cause_id][1] : '#03bdd6'} `, marginLeft: '16px'}}>{event.description}</p>
+            <p className="event-date">
+              {/* Date: {dateHandler(event.date, event.time).eventdate}  
+              {dateHandler(event.date, event.time).eventtime} */}
+            </p>
             {/* <p className="event-time">Time: {event.time}</p> */}
-            <p>Users Checked In: {event.checked_in_users}</p>
+            <p style={{ marginLeft: '20px', fontSize: "12px"}}>Users Checked In: {event.checked_in_users}</p>
+            <ShareThis description={event.description} title={event.title} image={event.img_link} /> 
             <div className="live-btn-container">
               <div>
                 <div className="live-btn-wrapper">
@@ -181,9 +185,10 @@ function Event2() {
             </div>
           </div>
         </div>
-        <h4>Live Streams : {livestreams.length} </h4>
+        <h4 style={{marginTop: '30px', marginBottom: '30px'}}><span class="bordered-text">Live Streams : {livestreams.length} </span></h4>
+        <container className="container d-flex flex-column overflow-auto">
+        {livestreams.map((live, index) => {
 
-        {livestreams.map((live) => {
           return (
             <div className="cardSize"
             // style={{marginLeft: '25%'}}
@@ -222,19 +227,46 @@ function Event2() {
       </div>
     </div>
     </div>
-        
+            // <div class="card mb-3" style={{ maxWidth: "540px" }}>
+            //   <div class="row g-3">
+            //     <div class="col-4 d-flex align-content-center">
+            //       <img
+            //         src="https://picsum.photos/600/400"
+            //         class="img-fluid rounded-start"
+            //         alt="..."
+            //       />
+            //     </div>
+            //     <div class="col-8">
+            //       <div class="card-body">
+            //         <h5 class="card-title">User Name</h5>
+            //         {/* <p class="card-text">
+            //           This is a wider card with supporting text
+            //         </p> */}
+
+            //         <p class="card-text">
+            //           <button
+            //             class="btn btn-primary"
+            //             onClick={() => joinLive(live.viewer_code)}
+            //           >
+            //             Join Live
+            //           </button>
+            //         </p>
+            //       </div>
+            //     </div>
+            //   </div>
+            // </div>
           );
         })}
+        </container>
       </div>
     </div>
-
 <>
     <DirectionService address={event.address} />
     </>
-
     </container>
     </>
   );
+
 }
 
 export default Event2;
