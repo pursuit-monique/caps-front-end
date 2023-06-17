dcc5320b111fef9134b3aa1a907f84953827fa3d
Monique <107360758+pursuit-monique@users.noreply.github.com>
2023-06-17 13:30:25 -0400

    Description change

--- src/pages/NewEvent.jsx
+++ src/pages/NewEvent.jsx
@@ -169,19 +169,6 @@ function NewEvent() {
               <option value="charity">Charity</option>
               <option value="community">Community</option>
               <option value="food">Food &amp; Beverage</option>
-
-              {/* <option value="">Select a category</option>
-              <option value="environmental-conservation">
-                Environmental Conservation
-              </option>
-              <option value="education">Education</option>
-              <option value="animal-rights">Animal Rights</option>
-              <option value="societal-justice">Societal Justice</option>
-              <option value="disability-rights">Disability Rights</option>
-              <option value="veterans-issues">Veterans Issues</option>
-              <option value="mental-health-awareness">
-                Mental Health Awareness
-              </option> */}
             </select>
           </div>
           <div className="row"></div>
@@ -197,113 +184,6 @@ function NewEvent() {
             }}
           />
 
-          {/* <div className="col-md-6">
-            <label htmlFor="address" className="form-label">
-              Address
-            </label>
-
-            <input
-              type="text"
-              className="form-control"
-              id="address"
-              placeholder="1234 Main St"
-              value={event.address}
-              onChange={(e) => setEvent({ ...event, address: e.target.value })}
-            />
-          </div>
-
-          <div className=" col-md-6">
-            <label htmlFor="city" className="form-label">
-              City
-            </label>
-
-            <input
-              type="text"
-              className="form-control"
-              id="city"
-              value={event.city}
-              onChange={(e) => setEvent({ ...event, city: e.target.value })}
-            />
-          </div>
-
-          <div className="col-md-6">
-            <label htmlFor="state" className="form-label">
-              State
-            </label>
-
-            <select
-              id="state"
-              className="form-select"
-              value={event.state}
-              onChange={(e) => setEvent({ ...event, state: e.target.value })}
-            >
-              <option value="">Choose a state</option>
-              <option value="AL">AL</option>
-              <option value="AK">AK</option>
-              <option value="AZ">AZ</option>
-              <option value="AR">AR</option>
-              <option value="CA">CA</option>
-              <option value="CO">CO</option>
-              <option value="CT">CT</option>
-              <option value="DE">DE</option>
-              <option value="FL">FL</option>
-              <option value="GA">GA</option>
-              <option value="HI">HI</option>
-              <option value="ID">ID</option>
-              <option value="IL">IL</option>
-              <option value="IN">IN</option>
-              <option value="IA">IA</option>
-              <option value="KS">KS</option>
-              <option value="KY">KY</option>
-              <option value="LA">LA</option>
-              <option value="ME">ME</option>
-              <option value="MD">MD</option>
-              <option value="MA">MA</option>
-              <option value="MI">MI</option>
-              <option value="MN">MN</option>
-              <option value="MS">MS</option>
-              <option value="MO">MO</option>
-              <option value="MT">MT</option>
-              <option value="NE">NE</option>
-              <option value="NV">NV</option>
-              <option value="NH">NH</option>
-              <option value="NJ">NJ</option>
-              <option value="NM">NM</option>
-              <option value="NY">NY</option>
-              <option value="NC">NC</option>
-              <option value="ND">ND</option>
-              <option value="OH">OH</option>
-              <option value="OK">OK</option>
-              <option value="OR">OR</option>
-              <option value="PA">PA</option>
-              <option value="RI">RI</option>
-              <option value="SC">SC</option>
-              <option value="SD">SD</option>
-              <option value="TN">TN</option>
-              <option value="TX">TX</option>
-              <option value="UT">UT</option>
-              <option value="VT">VT</option>
-              <option value="VA">VA</option>
-              <option value="WA">WA</option>
-              <option value="WV">WV</option>
-              <option value="WI">WI</option>
-              <option value="WY">WY</option>
-            </select>
-          </div>
-
-          <div className="col-md-6">
-            <label htmlFor="zip" className="form-label">
-              Zip
-            </label>
-
-            <input
-              type="text"
-              className="form-control"
-              id="zip"
-              value={event.zip}
-              onChange={(e) => setEvent({ ...event, zip: e.target.value })}
-            />
-          </div> */}
 
           <div className="col-6 col-md-6">
             <label htmlFor="date" className="form-label">
