0b61005b6a7aab22c907fb92efe94f99ccf724ac
Monique <107360758+pursuit-monique@users.noreply.github.com>
2023-06-19 19:53:29 -0400

    Revert "Fixes"

--- src/pages/LiveStream.jsx
+++ src/pages/LiveStream.jsx
@@ -1,35 +1,17 @@
-import { useParams, useNavigate } from "react-router-dom";
-import "./LiveStream.css";
+import { useParams } from "react-router-dom";
 
 export default function LiveStream() {
-  const navigate = useNavigate();
   const { roomCode } = useParams();
   const subDomain = process.env.REACT_APP_100MS_SUB_DOMAIN;
   const link = `https://${subDomain}/meeting/${roomCode}`;
 
-  function goBack() {
-    navigate(-2);
-  }
-
   return (
-    <div className="livestream-container">
-      <iframe
-        title="100ms-app"
-        allow="camera *;microphone *;display-capture *"
-        src={link}
-        className="iframe"
-        //   style={{ height: "900px", width: "900px" }}
-      ></iframe>
-      <div className="d-flex justify-content-center">
-        <button
-          type="button"
-          className="btn btn-sm btn-secondary"
-          onClick={goBack}
-          onTouchStart={goBack}
-        >
-          Back to Event
-        </button>
-      </div>
-    </div>
+    <iframe
+      title="100ms-app"
+      allow="camera *;microphone *;display-capture *"
+      src={link}
+      className="iframe"
+      //   style={{ height: "900px", width: "900px" }}
+    ></iframe>
   );
 }
